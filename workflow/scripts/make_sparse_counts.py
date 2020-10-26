496071d161295a04d4f1322e4ce75817ad6eb219
Allen Lynch <alynch@ds.dfci.harvard.edu>
2020-10-26 16:01:08 -0400

    changed notebook, added diff-diffpeak test

--- workflow/scripts/make_sparse_counts.py
+++ workflow/scripts/make_sparse_counts.py
@@ -4,7 +4,7 @@ from scipy import sparse
 import argparse
 import sys
 
-def main(barcodes, peaks, fragments, corrected = True):
+def main(barcodes, peaks, fragments, corrected = True, deduplicate=False):
 
     barcodes = [barcode.strip() for barcode in barcodes]
     barcode_ids = {barcode : id_ for id_, barcode in enumerate(barcodes)}
@@ -15,13 +15,20 @@ def main(barcodes, peaks, fragments, corrected = True):
 
     row, column, data = [],[],[]
     
+    dupgroups_encountered = {}
     for fragment in fragments:
 
         fragment = fragment.strip().split('\t')
         barcode = fragment[3]
         peak = '_'.join(fragment[10:13])
+        dupgroup = fragment[4]
 
-        if peak in peak_ids and barcode in barcode_ids:
+        add_count = not deduplicate
+        if deduplicate and (dupgroup == '-1' or not dupgroup in dupgroups_encountered):
+            dupgroups_encountered[dupgroup] = True
+            add_count = True
+
+        if add_count and peak in peak_ids and barcode in barcode_ids:
             row.append(barcode_ids[barcode])
             column.append(peak_ids[peak])
             if corrected:
@@ -42,12 +49,14 @@ if __name__ == "__main__":
     parser.add_argument('-b','--barcodes', type = argparse.FileType('r'), required = True)
     parser.add_argument('-p','--peaks',type=argparse.FileType('r'), required=True)
     parser.add_argument('-f','--fragments', type = argparse.FileType('r'), required=True)
-    parser.add_argument('--corrected',action='store_true')
+    parser.add_argument('--corrected',action='store_true', default=False)
+    parser.add_argument('--dedup',action='store_true',default=False)
     parser.add_argument('-o','--output',type = str, required=True)
 
     args = parser.parse_args()
 
-    count_matrix = main(args.barcodes, args.peaks, args.fragments, corrected=args.corrected)
+    count_matrix = main(args.barcodes, args.peaks, args.fragments,
+        corrected=args.corrected, deduplicate=args.dedup)
 
     print('Created count matrix:\n' + repr(count_matrix), file = sys.stderr)
     
