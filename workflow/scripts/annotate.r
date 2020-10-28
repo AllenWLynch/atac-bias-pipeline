a1f8db0c414bd9ed547a6132f88df1658bfcf891
Allen Lynch <alynch@ds.dfci.harvard.edu>
2020-10-28 13:41:30 -0400

    added peak annotations to analysis

--- workflow/scripts/annotate.r
+++ workflow/scripts/annotate.r
@@ -2,10 +2,11 @@
 
 args = commandArgs(trailingOnly=TRUE)
 
-if (length(args) == 0){
-  stop("Requires [bedfile]", call.=FALSE)
+if (length(args) == 2){
+  bedfile <- args[1]
+  output <- args[2]
 } else {
-  bedfile = args[1]
+  stop("Requires [bedfile] [output]", call.=FALSE)
 }
 
 library(ChIPseeker)
@@ -17,4 +18,4 @@ peaks <- readPeakFile(bedfile)
 
 annotations <- annotatePeak(peaks, TxDb=txdb, tssRegion=c(-3000, 3000), verbose=FALSE)
 
-write.table(annotations@anno, file = "", quote=FALSE, sep = "\t", row.names=FALSE)
+write.table(as.data.frame(annotations@anno), file = output, quote=FALSE, sep = "\t", row.names=FALSE)
